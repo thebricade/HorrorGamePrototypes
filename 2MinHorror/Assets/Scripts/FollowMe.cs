6f507919bf00953ee1e01e7128dd341933552485
Brianna <bs3864@nyu.edu>
2020-06-01 15:01:03 -0400

    Barn & Goats hopefully

---
+++ 2MinHorror/Assets/Scripts/FollowMe.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMe : MonoBehaviour
+{
+    public GameObject Goat1, Goat2;
+    public bool Goat1Selected, Goat2Selected; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Goat1Selected = false;
+        Goat2Selected = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
+            
+        }
+    }
+}
