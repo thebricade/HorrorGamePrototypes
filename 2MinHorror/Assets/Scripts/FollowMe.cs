670475f55729fa9d8c6d75e98aaab0a06c659c29
Brianna <bs3864@nyu.edu>
2020-06-08 12:59:59 -0400

    Goat Stares at you

--- 2MinHorror/Assets/Scripts/FollowMe.cs
+++ 2MinHorror/Assets/Scripts/FollowMe.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class FollowMe : MonoBehaviour
 {
-    public GameObject Goat1, Goat2;
-    public bool Goat1Selected, Goat2Selected; 
+    public Transform mTarget;
+    private float mSpeed;
+    private const float EPSILON = 0.1f;
+    
+    private int Speed;
+    public bool followingPlayer; 
     
     // Start is called before the first frame update
     void Start()
     {
-        Goat1Selected = false;
-        Goat2Selected = false; 
+        followingPlayer = false; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)) 
+        if (!followingPlayer)
         {
             
+        }
+        else
+        {
+            transform.LookAt(mTarget.position);
+            if ((transform.position - mTarget.position).magnitude > EPSILON)
+            {
+                transform.Translate(0f,0f,mSpeed*Time.deltaTime);
+            }
         }
     }
 }
